569c1427c9b50eb13efbb13094029338b8b4ae48
Joan  Viana <joanvianafons@gmail.com>
2016-01-08 22:40:36 +0000

    heroku deploy 0.81

--- web/app.php
+++ web/app.php
@@ -16,9 +16,9 @@ $apcLoader->register(true);
 */
 
 require_once __DIR__.'/../app/AppKernel.php';
-//require_once __DIR__.'/../app/AppCache.php';
+require_once __DIR__.'/../app/AppCache.php';
 
-$kernel = new AppKernel('prod', true);
+$kernel = new AppKernel('prod', false);
 $kernel->loadClassCache();
 //$kernel = new AppCache($kernel);
 
