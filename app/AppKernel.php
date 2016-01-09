635e47b00852916d81b17a2ad750311093681797
Joan  Viana <joanvianafons@gmail.com>
2016-01-09 22:11:05 +0000

    heroku deploy 0.92

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -18,8 +18,6 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new AppBundle\AppBundle(),
             new DWSBundle\DWSBundle(),
-            new Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle(),
-
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'), true)) {
@@ -27,6 +25,9 @@ class AppKernel extends Kernel
             $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
             $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
             $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
+            $bundles[] = new Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle();
+        }
+        if (in_array($this->getEnvironment(), array('prod'), true)) {
             $bundles[] = new Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle();
         }
 
